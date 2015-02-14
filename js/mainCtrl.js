3f0d19179627148ab8843342cf03930762ffec03
Rachel Welch <rachelmsmeltzer@gmail.com>
2015-02-14 10:47:30 -0700

    change javascript interval function to Angular interval

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,6 +1,6 @@
 var app = angular.module('chatroom');
 
-app.controller('mainCtrl', function($scope, parseService){
+app.controller('mainCtrl', function($scope, parseService, $interval){
   //In your controller you'll have a getParseData function and a postData function, but should be placed on $scope.
 
   //The getParseData function will call the getData method on the parseService object. You'll then save the result 
@@ -16,14 +16,15 @@ app.controller('mainCtrl', function($scope, parseService){
   //pass that text to the postData method on the parseService object which will then post it to the parse backend.
   
     $scope.postData = function() {
-      parseService.postData($scope.message)
+      parseService.postData($scope.message);
+      delete $scope.message;
     }
 
 
 
   //uncomment this code when your getParseData function is finished
   //This goes and gets new data every second, which mimicking a chat room experience.
-  setInterval(function(){
+  $interval(function(){
     $scope.getParseData();
   }, 1500)
 })
